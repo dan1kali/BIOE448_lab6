f8d6022ebe329f25e2f0e0cf0012d5e804c00c28
dan1kali <dl74@rice.edu>
2025-02-26 13:24:10 -0600

    declared flags and variables to store time data, and upper and lower thresholds

--- SignalProcessing/SignalProcessing.ino
+++ SignalProcessing/SignalProcessing.ino
@@ -2,6 +2,14 @@ const int sensor_pin = 0;
 int pulse_signal = 0;
 float BPM = 0;
 
+bool any_peak_detected = false;
+bool first_peak_detected = false;
+unsigned long first_pulse_time = 0;
+unsigned long second_pulse_time = 0;
+
+int upper_threshold = A;
+int lower_threshold = B;
+
 void setup() {
   Serial.begin(9600);
 
