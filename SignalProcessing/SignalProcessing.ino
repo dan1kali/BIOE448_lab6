e6410c6c62bf1dd189b42e084a2a900fe56941bc
dan1kali <dl74@rice.edu>
2025-02-26 13:50:46 -0600

    added checkpoints and flags to calculate pulse period

--- SignalProcessing/SignalProcessing.ino
+++ SignalProcessing/SignalProcessing.ino
@@ -1,4 +1,4 @@
-const int sensor_pin = 0;
+const int sensor_pin = 1;
 int pulse_signal = 0;
 float BPM = 0;
 
@@ -7,8 +7,8 @@ bool first_peak_detected = false;
 unsigned long first_pulse_time = 0;
 unsigned long second_pulse_time = 0;
 
-int upper_threshold = A;
-int lower_threshold = B;
+int upper_threshold = 820;
+int lower_threshold = 860;
 
 void setup() {
   Serial.begin(9600);
@@ -18,5 +18,35 @@ void setup() {
 void loop() {
   pulse_signal = analogRead(sensor_pin);
   Serial.println(pulse_signal);
+
+  delay(50);
+  
+
+
+  if (pulse_signal > upper_threshold && any_peak_detected ==
+  false) {
+  any_peak_detected = true;
+  // Do something about this peak
+
+    if (first_peak_detected == false) {
+      first_pulse_time = millis();
+
+      first_peak_detected == true //////////////////
+
+      } else {
+      second_pulse_time = millis();
+      pulse_period = second_pulse_time - first_pulse_time;
+
+      first_peak_detected == false //////////////
+
+      }
+
+  }
+
+  if (pulse_signal < lower_threshold) {
+  any_peak_detected = false;
+  }
+
+
 
 }
